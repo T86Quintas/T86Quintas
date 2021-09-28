62536ba28ff43c02316e6e1373c3681a82fd44d6
Micaela Cassandra Quintas <89237854+T86Quintas@users.noreply.github.com>
2021-09-28 13:26:10 +0800

    Add files via upload

---
+++ Quintas Micaela.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+   int var1=3000,var2=60,var3=360,result;
+
+   //solution
+   result = var1 / var2 * var3;
+   printf("%d divide %d multiply %d is %d", var1 , var2, var3, result);
+    
+    return 0;
+}
